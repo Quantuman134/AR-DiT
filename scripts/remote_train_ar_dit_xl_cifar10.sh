f60b9a977e1b6997c994db4bfda88df1e41e258a
Hongkun Zhang <hkzhang_tamu.edu@slogin-01.cm.cluster>
2026-07-22 17:53:03 -0500

    remote script and config file

---
+++ scripts/remote_train_ar_dit_xl_cifar10.sh
@@ -0,0 +1,6 @@
+
+source /scratch/project/prj-02-visual-ai/hkzhang/miniconda3/etc/profile.d/conda.sh
+conda activate dit
+cd /scratch/project/prj-02-visual-ai/hkzhang/AR-DiT
+
+bash scripts/train_cifar10.sh configs/train/cifar10_ar_dit_train.yaml
